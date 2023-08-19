fcf1273a3621600871f93c281f11e77fc33c971e
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-08-18 23:15:03 -0400

    vue

---
+++ src/components/Counter.vue
@@ -0,0 +1,18 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <h1>Counter - Vue</h1>
+    <h2>Direct access: {{ $store.state.count }}</h2>
+    <h2>Computed: {{ countComputed }}</h2>
+</template>
+
+
+<script>
+    export default{
+        
+        computed:{
+            countComputed(){
+                return this.$store.state.count
+            }
+        }
+    }
+</script>
