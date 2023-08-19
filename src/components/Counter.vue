0713e22a52bd2c3a8583138cc4814f97c680dd60
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-08-19 11:12:51 -0400

    leer state activo

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -3,16 +3,25 @@
     <h1>Counter - Vue</h1>
     <h2>Direct access: {{ $store.state.count }}</h2>
     <h2>Computed: {{ countComputed }}</h2>
+
+    <h1>mapState</h1>
+    <h2>mapState: {{ count }}</h2>
 </template>
 
 
 <script>
+
+    import { mapState } from 'vuex'
     export default{
-        
+
         computed:{
             countComputed(){
                 return this.$store.state.count
-            }
+            },
+           /// ... mapState(['count'])
+           ... mapState({
+            count: state =>state.count
+           })
         }
     }
 </script>
